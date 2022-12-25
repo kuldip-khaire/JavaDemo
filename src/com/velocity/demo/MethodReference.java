08576016bbec42b038f7ec09af0ecaaf3349069a
Kuldip <kuldipkhaire27@gmail.com>
2022-12-25 14:59:24 +0530

    third commit : Method referenece code is added

---
+++ src/com/velocity/demo/MethodReference.java
@@ -0,0 +1,23 @@
+package com.velocity.demo;
+
+interface Sayable {
+	void saySomething();
+}
+
+public class MethodReference {
+
+	public void sayMore() {
+		System.out.println("Hello, I am in non static method");
+	}
+
+	public static void main(String[] args) {
+		
+		MethodReference mr = new MethodReference();
+		
+		Sayable s = mr :: sayMore;
+		
+		s.saySomething();
+
+	}
+
+}
